e30d10f8786c69fae8a5738765ab96fe8e4e514d
fetr <fetr9415694@gmail.com>
2024-05-26 14:02:11 +0300

    Добавлены укороченные версии БД, реализован класс Movie

---
+++ Movie.py
@@ -0,0 +1,6 @@
+class Movie:
+    def __init__(self, name, director, rating, genres):
+        self.name = name
+        self.director = director
+        self.rating = rating
+        self.genres = genres
